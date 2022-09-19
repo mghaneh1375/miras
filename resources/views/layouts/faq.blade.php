bee8ba675d895d27ac7a6fbffa2acd9cfbb4c8d2
Alborz <alborzhacker@gmail.com>
2022-09-19 11:41:47 +0430

    Slider and faq

---
+++ resources/views/layouts/faq.blade.php
@@ -0,0 +1,61 @@
+            <div class="container">
+                <div class="ui-box bg-white">
+                     <div class="ui-box-content">
+                            <div class="accordion" id="accordionExample">
+                            <!-- start of accordion-item -->
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingOne">
+                                    <button class="accordion-button collapsed padding0" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                        چگونه می توانم سفارش خود را ثبت کنم ؟
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        شما برای ثبت سفارش و خرید از فروشگاه اینترنتی یکتاکالا پس از اینکه کالای مورد
+                                        نظر
+                                        را انتخاب کردید آن را به سبد خرید اضافه و به ترتیب مراحل خرید اینترنتی را تکمیل
+                                        نمایید .
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end of accordion-item -->
+                            <!-- start of accordion-item -->
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingTwo">
+                                    <button class="accordion-button collapsed padding0" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        چگونه شماره شبای حساب بانکی را به دست آوریم؟
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        برای دریافت شماره شبا از طریق شماره حساب و یا شماره کارت خود، وارد سایت بانک
+                                        مربوطه شوید.
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end of accordion-item -->
+                            <!-- start of accordion-item -->
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingThree">
+                                    <button class="accordion-button collapsed padding0" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseThree" aria-expanded="false"
+                                        aria-controls="collapseThree">
+                                        می توانم سفارشم را تلفنی ثبت کنم ؟
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" class="accordion-collapse collapse"
+                                    aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        خیر، امکان ثبت سفارش بصورت تلفنی امکان پذیر نیست.
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end of accordion-item -->
+                        </div>
+                     </div>
+                </div>
+            </div>
