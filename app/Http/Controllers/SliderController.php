eff5a9b7f95572a87c14d8ca214be84a97f48348
mghaneh1375 <mghaneh1375@gmail.com>
2022-09-20 11:32:46 +0430

    slider controller added

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -2,9 +2,167 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SliderResource;
+use App\Models\Slider;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class SliderController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {    
+        if($request->user() != null && 
+            (
+                $request->user()->level == User::$ADMIN_LEVEL ||
+                $request->user()->level == User::$EDITOR_LEVEL 
+            )
+        ) {
+            $items = SliderResource::collection(Slider::all())->toArray($request);
+            return view('admin.slider.list', compact('items'));
+        }
+
+        return SliderResource::collection(Slider::visible()->get())->additional(['status' => 'ok']);   
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.slider.create');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = [
+            'alt' => 'nullable|string|min:2',
+            'href' => 'nullable|string|url',
+            'image_file_large' => 'required|image',
+            'image_file_mid' => 'nullable|image',
+            'image_file_small' => 'nullable|image',
+            'priority' => 'required|integer',
+            'visibility' => 'nullable|boolean',
+        ];
+        
+        if(self::hasAnyExcept(array_keys($validator), $request->keys()))
+            abort(401);
+
+        $request->validate($validator);
+
+        $request['img_large'] = $request->image_file_large->store('public/slider');
+        $request['img_large'] = str_replace('public/slider/', '', $request['img_large']);
+        
+        if($request->has('image_file_mid')) {
+            $request['img_mid'] = $request->image_file_mid->store('public/slider');
+            $request['img_mid'] = str_replace('public/slider/', '', $request['img_mid']);
+        }
+        
+        if($request->has('image_file_small')) {
+            $request['img_small'] = $request->image_file_small->store('public/slider');
+            $request['img_small'] = str_replace('public/slider/', '', $request['img_small']);
+        }
+
+        slider::create($request->toArray());
+        return Redirect::route('slider.index');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Slider $slider)
+    {
+        $validator = [
+            'alt' => 'nullable|string|min:2',
+            'href' => 'nullable|url',
+            'image_file_large' => 'nullable|image',
+            'image_file_mid' => 'nullable|image',
+            'image_file_small' => 'nullable|image',
+            'priority' => 'nullable|integer',
+            'visibility' => 'nullable|boolean',
+        ];
+        
+        if(self::hasAnyExcept(array_keys($validator), $request->keys()))
+            abort(401);
+
+        $request->validate($validator);
+
+        if($request->has('image_file_large')) {
+            $filename = $request->image_file_large->store('public/slider');
+            $filename = str_replace('public/slider/', '', $filename);
+
+            if(file_exists(__DIR__ . '/../../../public/storage/slider/' . $slider->img_large))
+                unlink(__DIR__ . '/../../../public/storage/slider/' . $slider->img_large);
+            
+            $slider->img_large = $filename;
+        }
+
+        if($request->has('image_file_mid')) {
+            $filename = $request->image_file_mid->store('public/slider');
+            $filename = str_replace('public/slider/', '', $filename);
+            
+            if($slider->img_mid != null && file_exists(__DIR__ . '/../../../public/storage/slider/' . $slider->img_mid))
+                unlink(__DIR__ . '/../../../public/storage/slider/' . $slider->img_mid);
+            
+            $slider->img_mid = $filename;
+        }
+        
+        if($request->has('image_file_small')) {
+            $filename = $request->image_file_small->store('public/slider');
+            $filename = str_replace('public/slider/', '', $filename);
+            
+            if($slider->img_small != null && file_exists(__DIR__ . '/../../../public/storage/slider/' . $slider->img_small))
+                unlink(__DIR__ . '/../../../public/storage/slider/' . $slider->img_small);
+            
+            $slider->img_small = $filename;
+        }
+
+        $slider->href = $request->has('href') ? $request['href'] : $slider->href;
+        $slider->alt = $request->has('alt') ? $request['alt'] : $slider->alt;
+        $slider->visibility = $request->has('visibility') ? $request['visibility'] : $slider->visibility;
+        $slider->priority = $request->has('priority') ? $request['priority'] : $slider->priority;
+
+        $slider->save();
+        return Redirect::route('slider.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Slider $slider)
+    {
+        if(file_exists(__DIR__ . '/../../../public/storage/slider/' . $slider->img_large))
+            unlink(__DIR__ . '/../../../public/storage/slider/' . $slider->img_large);
+
+        if($slider->img_mid != null && !empty($slider->img_mid) && file_exists(__DIR__ . '/../../../public/storage/slider/' . $slider->img_mid))
+            unlink(__DIR__ . '/../../../public/storage/slider/' . $slider->img_mid);
+
+        if($slider->img_small != null && !empty($slider->img_small) && file_exists(__DIR__ . '/../../../public/storage/slider/' . $slider->img_small))
+            unlink(__DIR__ . '/../../../public/storage/slider/' . $slider->img_small);
+
+        $slider->delete();
+        return response()->json(['status' => 'ok']);
+    }
 }
--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Slider extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'id',
+        'img_large',
+        'img_mid',
+        'img_small',
+        'href',
+        'alt',
+        'priority',
+        'visibility',
+    ];
+
+    public function scopeVisible($query) {
+        return $query->where('visibility', true);
+    }
+
 }
