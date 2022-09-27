6d700dfc8420db222be898a2251a8c26c1922ba9
alborz-moon <alborzmoon@gmail.com>
2022-09-27 11:56:33 +0330

    box

--- resources/views/layouts/box.blade.php
+++ resources/views/layouts/box.blade.php
@@ -7,9 +7,9 @@
                             <!-- Additional required wrapper -->
                             <div class="swiper-wrapper">
                                 <!-- Slides -->
-                                <div class="swiper-slide customBorderBoxShadow">
+                                <div class="swiper-slide">
                                     <!-- start of product-card -->
-                                    <div class="product-card">
+                                    <div class="product-card customBorderBoxShadow">
                                         <div class="product-thumbnail">
                                             <a href="#">
                                                 <img src="{{ asset('theme-assets/images/box/pic1.png') }}" alt="product title">
@@ -29,7 +29,42 @@
                                             <div class="product-price fa-num">
                                                 <div class="d-flex align-items-center">
                                                     <span class="fontSize15 pl-10 position-relative"><img src="{{ asset('theme-assets/images/svg/off.svg') }}" alt=""><span class="position-absolute fontSize10 colorWhite r-0 customOff">20%</span></span>
-                                                    <del class="price-old textColor fontSize15">26,900,000</del>
+                                                    <del class="customlineText textColor fontSize15">26,900,000</del>
+                                                </div>
+                                                <div class="fontSize20">26,249,000 <span
+                                                        class="fontSize20 colorYellow">ت</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="product-card-footer mb-2">
+                                            <div class="textColor">از تهران دستی</div>
+                                        </div>
+                                    </div>
+                                    <!-- end of product-card -->
+                                </div>
+                                <div class="swiper-slide">
+                                    <!-- start of product-card -->
+                                    <div class="product-card customBorderBoxShadow">
+                                        <div class="product-thumbnail">
+                                            <a href="#">
+                                                <img src="{{ asset('theme-assets/images/box/pic1.png') }}" alt="product title">
+                                            </a>
+                                        </div>
+                                        <div class="product-card-body">
+                                            <h2 class="product-title">
+                                                <a class="textColor fontSize12" href="#">سبد خرید با در چهار رنگ متفاوت سبد حصیری فامیلیران مدل تولک درب‌دار</a>
+                                            </h2>
+                                            <div class="product-variant">
+                                                <span class="colorWhite customBoxLabel">خاتم کاری</span>
+                                            </div>
+                                            <div class="spaceBetween mt-3 mb-3">
+                                                <span class="fontSize12 colorRed">موجودی تنها دو عدد</span>
+                                                <span><img src="{{ asset('theme-assets/images/svg/star.svg') }}" alt=""><img src="{{ asset('theme-assets/images/svg/star.svg') }}" alt=""><img src="{{ asset('theme-assets/images/svg/star.svg') }}" alt=""><img src="{{ asset('theme-assets/images/svg/star.svg') }}" alt=""><img src="{{ asset('theme-assets/images/svg/star.svg') }}" alt=""></span>
+                                            </div>
+                                            <div class="product-price fa-num">
+                                                <div class="d-flex align-items-center">
+                                                    <span class="fontSize15 pl-10 position-relative"><img src="{{ asset('theme-assets/images/svg/off.svg') }}" alt=""><span class="position-absolute fontSize10 colorWhite r-0 customOff">20%</span></span>
+                                                    <del class="customlineText textColor fontSize15">26,900,000</del>
                                                 </div>
                                                 <div class="fontSize20">26,249,000 <span
                                                         class="fontSize20 colorYellow">ت</span>
